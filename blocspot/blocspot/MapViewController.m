38bc0109b7c3ea099992523ab2c6cb124e879429
Donald Dixon <dixondonald@gmail.com>
2016-05-26 12:51:48 -0400

    search works

--- blocspot/blocspot/MapViewController.m
+++ blocspot/blocspot/MapViewController.m
@@ -1,27 +1,88 @@
 //
-//  ViewController.m
+//  MapViewController.m
 //  blocspot
 //
-//  Created by Ziggy on 5/5/16.
+//  Created by Ziggy on 5/12/16.
 //  Copyright © 2016 DonaldDixon. All rights reserved.
 //
 
 #import "MapViewController.h"
+#import "MapKit/Mapkit.h"
 
-@interface MapViewController ()
+@interface MapViewController () <MKMapViewDelegate, UITextFieldDelegate, CLLocationManagerDelegate>
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) IBOutlet UITextField *searchTextField;
 
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) NSMutableArray *matchingItems;
 @end
 
 @implementation MapViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.searchTextField.delegate = self;
+    self.mapView.delegate = self;
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
+    
+    self.mapView.showsUserLocation = YES;
+    
 }
+
+
+- (IBAction)didSearch:(id)sender {
+    [self.searchTextField resignFirstResponder];
+    [self.mapView removeAnnotations:[self.mapView annotations]];
+    [self doSearch];
+}
+
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    MKCoordinateRegion mapRegion;
+    mapRegion.center = mapView.userLocation.coordinate;
+    mapRegion.span.latitudeDelta = 0.2;
+    mapRegion.span.longitudeDelta = 0.2;
+    
+    [mapView setRegion:mapRegion animated: YES];
+}
+
+
+- (void) doSearch {
+    MKLocalSearchRequest *request =
+    [[MKLocalSearchRequest alloc] init];
+    request.naturalLanguageQuery = self.searchTextField.text;
+    request.region = _mapView.region;
+    
+        self.matchingItems = [[NSMutableArray alloc] init];
+    
+    MKLocalSearch *search =
+    [[MKLocalSearch alloc]initWithRequest:request];
+    
+    [search startWithCompletionHandler:^(MKLocalSearchResponse
+                                         *response, NSError *error) {
+        if (response.mapItems.count == 0)
+            NSLog(@"No Matches");
+        else
+            for (MKMapItem *item in response.mapItems)
+            {
+                                [self.matchingItems addObject:item];
+                MKPointAnnotation *annotation =
+                [[MKPointAnnotation alloc]init];
+                annotation.coordinate = item.placemark.coordinate;
+                annotation.title = item.name;
+                [self.mapView addAnnotation:annotation];
+            }
+    }];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
 
 @end
